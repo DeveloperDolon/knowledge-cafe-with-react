d9ff8e550964cc80d4e3004a6c1d582f70b2ce7d
Dolon Roy <dolonr718@gmail.com>
2023-09-12 10:16:46 +0600

    starting commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { FaBeer } from 'react-icons/fa';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello world. <FaBeer></FaBeer></h1>
+      </div>
+    </>
+  )
+}
+
+export default App
